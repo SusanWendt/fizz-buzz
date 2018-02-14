14f02255ccf2e9a35cab4b3156ec38e2b36aebb0
SusanWendt <wendt.37@osu.edu>
2018-02-14 14:58:52 -0500

    count renamed

--- src/main/java/fizzbuzz/FizzBuzzApp.java
+++ src/main/java/fizzbuzz/FizzBuzzApp.java
@@ -4,11 +4,11 @@ public class FizzBuzzApp {
 
 	public static void main(String[] args) {
 		FizzBuzz myFizzBuzz = new FizzBuzz(); 
-		int number = 0;
+		int count = 0;
 		for (int i = 0; i <= 100 ; i++) {
 
-		System.out.println(myFizzBuzz.say(number));
-		number++; 
+		System.out.println(myFizzBuzz.say(count));
+		count++; 
 		}
 	}
 }
